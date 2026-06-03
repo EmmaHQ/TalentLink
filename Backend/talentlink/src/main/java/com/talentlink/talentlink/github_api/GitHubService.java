742db9cbcce597f33d780909b1189c98551983a9
EmmaHQ <emmanuel_hernandez82232@elpoli.edu.co>
2026-06-03 13:14:17 -0500

    arreglo back

--- Backend/talentlink/src/main/java/com/talentlink/talentlink/github_api/GitHubService.java
+++ Backend/talentlink/src/main/java/com/talentlink/talentlink/github_api/GitHubService.java
@@ -1,21 +1,26 @@
-package com.talentlink.talentlink.github_api;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import java.util.Map;
 
 @Service
 public class GitHubService {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     public Object fetchGitHubData(String username) {
 
-        RestTemplate restTemplate = new RestTemplate();
+        try {
+            String url = "https://api.github.com/users/" + username;
 
-        String url =
-                "https://api.github.com/users/" + username;
+            return restTemplate.getForObject(url, Object.class);
 
-        return restTemplate.getForObject(
-                url,
-                Object.class
-        );
+        } catch (Exception e) {
+            System.out.println("ERROR GitHub API: " + e.getMessage());
+
+            return Map.of(
+                "error", "No se pudo obtener el usuario de GitHub",
+                "username", username
+            );
+        }
     }
 }
