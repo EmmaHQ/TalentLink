2b2705e270397368231d1a7e17680f98e86a401c
EmmaHQ <emmanuel_hernandez82232@elpoli.edu.co>
2026-06-02 22:41:57 -0500

    Api nueva

--- Backend/talentlink/src/main/java/com/talentlink/talentlink/config/CorsConfig.java
+++ Backend/talentlink/src/main/java/com/talentlink/talentlink/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "http://localhost:3000"
+                                "http://localhost:3000",
+                                "https://talentlink-steel.vercel.app"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*");
