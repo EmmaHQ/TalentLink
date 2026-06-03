1a3219cdb28dfd6c6d997e379311bdc2f642729c
EmmaHQ <emmanuel_hernandez82232@elpoli.edu.co>
2026-06-02 22:47:18 -0500

    final

--- Backend/talentlink/src/main/java/com/talentlink/talentlink/config/CorsConfig.java
+++ Backend/talentlink/src/main/java/com/talentlink/talentlink/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                         .allowedOrigins(
                                 "http://localhost:5173",
                                 "http://localhost:3000",
-                                "https://talentlink-steel.vercel.app"
+                                "https://talentlink-git-main-emma-hq-s-projects.vercel.app"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*");
